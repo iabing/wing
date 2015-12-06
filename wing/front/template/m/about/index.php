39bd276394f00b35035772cfcff5dd962925987b
dev <1115615218@qq.com>
2015-12-06 11:27:15 +0800

    menu json

--- wing/front/template/m/about/index.php
+++ wing/front/template/m/about/index.php
@@ -1,16 +1,1 @@
-<!doctype html>
-<html>
-<head>
-<title>关于</title>
-<?php load_template('inc/cdn'); ?>
-</head>
-<body>
-<div class="m-page">
-  <?php load_template('inc/header'); ?>
-  <div>
-    hello ui
-  </div>
-  <?php load_template('inc/navbar'); ?>
-</div>
-</body>
-</html>
+<?php redirect('/about/team'); ?>
---
+++ wing/front/template/m/about/team.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<title>关于九号</title>
+<?php load_template('inc/cdn'); ?>
+</head>
+<body>
+<div class="m-page">
+  <?php load_template('inc/header'); ?>
+  <div>
+    hello ui
+  </div>
+  <?php load_template('inc/navbar'); ?>
+</div>
+</body>
+</html>
